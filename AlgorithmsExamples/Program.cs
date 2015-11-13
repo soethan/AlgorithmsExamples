beaa4c985a5f3a656ca7c540d944d9e26b2333a1
soethan <soethan@gmail.com>
2015-11-13 13:06:27 +0800

    QuickSort

---
+++ AlgorithmsExamples/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nProgram for sorting a numeric array using Quick Sorting");
+            Console.Write("\n\nEnter number of elements: ");
+
+            int totalElements = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[totalElements];
+
+            for (int i = 0; i < totalElements; i++)
+            {
+                Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Input int array  : ");
+            Console.Write("\n");
+
+            for (int k = 0; k < totalElements; k++)
+            {
+                Console.Write(numbers[k] + " ");
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("QuickSort By Recursive Method");
+
+            var quickSort = new QuickSort();
+            quickSort.Sort(numbers, 0, totalElements - 1);
+
+            for (int i = 0; i < totalElements; i++)
+                Console.WriteLine(numbers[i]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AlgorithmsExamples/QuickSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsExamples
+{
+    public class QuickSort
+    {
+        //6  5  9  12  3  
+        //6  5  3  12  9  (pivot index is 3)
+        public int Partition(int[] numbers, int left, int right)
+        {
+            int pivot = numbers[left];
+            while (true)
+            {
+                while (numbers[left] < pivot)
+                    left++;
+
+                while (numbers[right] > pivot)
+                    right--;
+
+                if (left < right)
+                {
+                    int temp = numbers[right];
+                    numbers[right] = numbers[left];
+                    numbers[left] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+        public void Sort(int[] arr, int left, int right)
+        {
+            // For Recusrion  
+            if (left < right)
+            {
+                int pivotIndex = Partition(arr, left, right);
+
+                if (pivotIndex > 1)
+                    Sort(arr, left, pivotIndex - 1);
+
+                if (pivotIndex + 1 < right)
+                    Sort(arr, pivotIndex + 1, right);
+            }
+        }
+    }
+}
