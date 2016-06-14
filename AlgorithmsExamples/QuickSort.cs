1fe097ca4f1259a54f9b7a617d38cde1eb2806e5
soethan <soethan@gmail.com>
2016-06-14 15:12:18 +0800

    update comment

--- AlgorithmsExamples/QuickSort.cs
+++ AlgorithmsExamples/QuickSort.cs
@@ -8,8 +8,11 @@ namespace AlgorithmsExamples
 {
     public class QuickSort : ISort
     {
-        //6  5  9  12  3  
-        //6  5  3  12  9  (pivot index is 3)
+        //6  5  9  12  3
+        //3  5  9  12  6
+        //3  5  6  12  9 (pivot index is 2)
+        //values on the left are always less than pivot value while values on the right are always greater than pivot value.
+
         public int Partition(int[] numbers, int left, int right)
         {
             int pivot = numbers[left];
@@ -36,7 +39,7 @@ namespace AlgorithmsExamples
 
         public void Sort(int[] arr, int left, int right)
         {
-            // For Recusrion  
+            // For Recursion  
             if (left < right)
             {
                 int pivotIndex = Partition(arr, left, right);
