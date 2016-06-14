366204db4ea096fe0b417401730022061e558c7d
soethan <soethan@gmail.com>
2016-06-14 16:57:00 +0800

    refined BubbleSort

--- AlgorithmsExamples/BubbleSort.cs
+++ AlgorithmsExamples/BubbleSort.cs
@@ -15,7 +15,7 @@ namespace AlgorithmsExamples
 
             for (int i = 0; i < numbers.Length; i++)
             {
-                for (int j = 0; j < numbers.Length - 1; j++)
+                for (int j = 0; j < numbers.Length - i - 1; j++)
                 {
                     if (numbers[j] > numbers[j + 1])
                     {
