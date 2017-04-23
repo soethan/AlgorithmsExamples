e74e1507522c8366c8d772b40b184f2b6059c2e4
Soe Than <soethan@gmail.com>
2017-04-23 12:08:29 +0800

    BubbleSort refactor

--- AlgorithmsExamples/BubbleSort.cs
+++ AlgorithmsExamples/BubbleSort.cs
@@ -6,25 +6,38 @@ using System.Threading.Tasks;
 
 namespace AlgorithmsExamples
 {
+    /// <summary>
+    /// On Each Pass
+    ///     Compare each array item to it’s right neighbor
+    ///     If the right neighbor is smaller then Swap right and left
+    ///     Repeat for the remaining array items
+    /// Perform subsequent passes until no swaps are performed
+    /// Worst Case: O(n^2)
+    /// Best Case: O(n)
+    /// Space Requirement: O(n)
+    /// </summary>
     public class BubbleSort: ISort
     {
         // 5  4  3  2  1
         public void Sort(int[] numbers)
         {
-            int temp = 0;
+            bool swapped;
 
-            for (int i = 1; i <= numbers.Length; i++)
+            do
             {
-                for (int j = 0; j < numbers.Length - i; j++)
+                swapped = false;
+                for (int i = 1; i < numbers.Length; i++)
                 {
-                    if (numbers[j] > numbers[j + 1])
+                    //left > right ==> swap
+                    if (numbers[i - 1] > numbers[i])
                     {
-                        temp = numbers[j + 1];
-                        numbers[j + 1] = numbers[j];
-                        numbers[j] = temp;
+                        var temp = numbers[i];
+                        numbers[i] = numbers[i - 1];
+                        numbers[i - 1] = temp;
+                        swapped = true;
                     }
                 }
-            }
+            } while (swapped != false);
         }
     }
 }
